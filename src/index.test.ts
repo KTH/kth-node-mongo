28f34db995bcc4abcc2e280d175aa1accf852e6a
Emanuel Bodin <embodin@kth.se>
2023-03-07 08:51:41 +0100

    fixed tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,15 +1,4 @@
-import { connect, isOk } from './index'
-
-jest.mock(
-  'mongoose',
-  jest.fn(() => ({
-    connect: jest.fn(() => {}),
-    connection: {
-      on: jest.fn(() => {}),
-    },
-  }))
-)
-const mockLog = {
+jest.mock('@kth/log', () => ({
   init: jest.fn(() => {}),
   child: jest.fn(() => ({
     debug: jest.fn(),
@@ -21,35 +10,38 @@ const mockLog = {
   error: jest.fn(),
   fatal: jest.fn(),
   info: jest.fn(),
+}))
+
+const mockMoongoose = {
+  connect: jest.fn(() => {}),
+  connection: {
+    on: jest.fn(() => {}),
+  },
 }
-
 describe('Test kth-node-mongo connection process', () => {
-  jest.mock('@kth/log', () => mockLog)
-
   beforeEach(() => {
     jest.clearAllMocks()
   })
+
   afterAll(() => jest.resetAllMocks())
 
   test('should call the connect method and register event handlers', async () => {
-    const connectOptions = {
-      logger: undefined,
-      dbUri: '',
-      mongooseDebug: false,
-    }
-    await connect(connectOptions)
-    //expect(mockLog.child).toHaveBeenCalled()
-    //expect(mockMoongoose.connection.on).toHaveBeenCalledWith('connected')
-    //expect(logSpy).toHaveBeenCalledWith('DATABASE: Connecting database...');
+    jest.mock('mongoose', () => mockMoongoose)
+    const { connect } = require('./index')
 
-    //expect(mockMoongoose.connect).toHaveBeenCalledTimes(1)
-    /* expect(mockMoongoose.connection.on).toHaveBeenCalledWith('connected')
-    expect(mockMoongoose.connection.on).toHaveBeenCalledWith('reconnected')
-    expect(mockMoongoose.connection.on).toHaveBeenCalledWith('disconnected')
-    expect(mockMoongoose.connection.on).toHaveBeenCalledWith('error') */
+    await connect({})
+
+    expect(mockMoongoose.connect).toHaveBeenCalledTimes(1)
+    expect(mockMoongoose.connection.on).toHaveBeenCalledWith('connected', expect.toBeFunction())
+    expect(mockMoongoose.connection.on).toHaveBeenCalledWith('reconnected', expect.toBeFunction())
+    expect(mockMoongoose.connection.on).toHaveBeenCalledWith('disconnected', expect.toBeFunction())
+    expect(mockMoongoose.connection.on).toHaveBeenCalledWith('error', expect.toBeFunction())
   })
 
   test('When no connection is initiated, isok status should be false', async () => {
+    jest.mock('mongoose', () => mockMoongoose)
+    const { isOk } = require('./index')
+
     const ok = await isOk()
     expect(ok).toBeFalsy()
   })
